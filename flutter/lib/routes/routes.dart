f9e478594b0978fb511b616db7368677716f3818
Jun Furuya <jun.fuluya@gmail.com>
2025-05-22 16:53:08 +0900

    多言語対応

--- flutter/lib/routes/routes.dart
+++ flutter/lib/routes/routes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../main.dart';
 import '../pages/ancient_documents/top.dart';
@@ -6,12 +8,23 @@ import '../pages/ancient_documents/top.dart';
 /// 画面遷移管理クラス
 ///
 class Routes {
+  /// ルーティング情報オブジェクトを取得する
   static MaterialApp getRoutes() {
     return MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ja', ''),
+        const Locale('en', ''),
+      ],
       initialRoute: '/',
       routes: <String, WidgetBuilder> {
         // FIXME 画面タイトル
-        '/': (BuildContext context) => MyHomePage(title: 'initial page'),
+        '/': (BuildContext context) => MyHomePage(title: AppLocalizations.of(context)!.app_title,),
         '/ancient_documents': (BuildContext context) => AncientDocumentsTop(title: 'page A')
         //'/ancient_documents/photo/camera': (BuildContext context) => MyPage(title: 'page B'),
         //'/ancient_documents/photo/': (BuildContext context) => MyPage(title: 'page B'),
