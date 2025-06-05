9ffd85d587d939b1be32c2d3889e38d61c6c76de
Jun Furuya <jun.fuluya@gmail.com>
2025-06-05 18:29:45 +0900

    撮った写真をローカルに保存できるようにする

--- flutter/lib/states/ancient_documents.dart
+++ flutter/lib/states/ancient_documents.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 import '../pages/ancient_documents.dart';
 import '../ui/common_parts.dart';
 
@@ -10,15 +12,33 @@ class AncientDocumentsCameraState extends State<AncientDocumentsCamera> {
   File? _image;
   final ImagePicker _picker = ImagePicker();
 
-  Future<void> _takePhoto() async {
+  Future<void> _takeAndSavePhoto() async {
     final XFile? pickedFile =
     await _picker.pickImage(source: ImageSource.camera);
 
-    if (pickedFile != null) {
+    if (pickedFile == null) {
+      return;
+    } else {
+      // アプリのドキュメントディレクトリ取得
+      final Directory appDir = await getApplicationDocumentsDirectory();
+
+      // 保存先のパスを作成（同じファイル名で保存）
+      final String fileName = path.basename(pickedFile.path);
+      final File savedImage = await File(pickedFile.path).copy('${appDir.path}/$fileName');
+
+      // UI更新
       setState(() {
-        _image = File(pickedFile.path);
+        _image = savedImage;
       });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('保存しました: ${savedImage.path}')),
+      );
     }
+
+    setState(() {
+      _image = File(pickedFile.path);
+    });
   }
 
   // 画面構築
@@ -35,7 +55,7 @@ class AncientDocumentsCameraState extends State<AncientDocumentsCamera> {
                 : Text('写真が選択されていません'),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _takePhoto,
+              onPressed: _takeAndSavePhoto,
               child: Text('カメラを起動する'),
             ),
           ],
